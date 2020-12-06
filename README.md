c1e85a85b5f9f84f4291fa5d660d561280da49e6
Inu-chan <60871376+Inu-chan@users.noreply.github.com>
2020-12-06 14:45:27 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# Maze-Game
-一个迷宫小游戏
+## 游戏简介
+本游戏由ZZH、WHY、FJY共同开发
+
+若您在游玩过程中遇到问题，或者有宝贵的建议，请[QQ联系](http://wpa.qq.com/msgrd?v=3&uin=2270840074&site=qq&menu=yes "QQ联系")
+
+Have Fun!
+## 环境需求
+#### 一、python 3.6+
+
+[官方下载](https://www.python.org/downloads/ "官方下载")
+
+#### 二、依赖的外部库：numpy、 easygui 和 pygame
+
+下载方式：
+1. 打开命令行（win10系统可以在左下角搜索栏输入`cmd`并按回车打开，如图所示）
+
+	![](https://ftp.bmp.ovh/imgs/2020/12/78da8c536f4a9fe9.png)
+
+2. 输入`pip install numpy`并按回车，如图所示
+
+	![](https://ftp.bmp.ovh/imgs/2020/12/74b7b62e47abc0de.png)
+
+3. 输入`pip install easygui`并按回车
+
+4. 输入`pip install pygame`并按回车
+
+## 游玩说明
+
+双击`start_game.py`运行游戏
+
+首先选择是否开启传送门模式、是否路径唯一：
+
+![](https://ftp.bmp.ovh/imgs/2020/12/b06c6fed2b1c19ad.png)
+
+然后查看游玩说明：
+
+![](https://ftp.bmp.ovh/imgs/2020/12/3cdbf4f43e51d65b.png)
+
+在游戏界面，您需要操纵角色，在1分30秒内从起点走到终点，且需要吃到足够数量的铜锣烧。剩余时间和当前铜锣烧数量将在游戏界面的右上角显示。
+
+![](https://ftp.bmp.ovh/imgs/2020/12/d4b2b15250d107a8.png)
+
+吃到足够的铜锣烧并且在时间结束之前到达终点即可过关。
+
+![](https://ftp.bmp.ovh/imgs/2020/12/961346bb96468bf8.png)
+
+如果时间到了未能过关，您将会听到“o泡时间到”，因为您是憨憨。
---
+++ dataStructures.py
@@ -0,0 +1,132 @@
+# coding=gbk
+
+class Deque:
+    def __init__(self, datas=None):
+        if datas is None:
+            datas = []
+        self.data = list(datas)
+        self.size = len(self.data)
+
+    def push_back(self, num):
+        self.data.append(num)
+        self.size += 1
+
+    def pop_back(self):
+        if self.size == 0:
+            raise RuntimeError
+        self.size -= 1
+        return self.data.pop(self.size)
+
+    def push_front(self, num):
+        self.data.insert(0, num)
+        self.size += 1
+
+    def pop_front(self):
+        if self.size == 0:
+            raise RuntimeError
+        self.size -= 1
+        return self.data.pop(0)
+
+    def empty(self):
+        return self.size == 0
+
+    def back(self):
+        return self.data[self.size]
+
+    def front(self):
+        return self.data[0]
+
+    def __len__(self):
+        return self.size
+
+class Stack:
+    def __init__(self, datas=None):
+        self.data = list(datas)
+        self.size = len(self.data)
+
+    def push(self, num):
+        self.data.append(num)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            raise RuntimeError
+        self.size -= 1
+        return self.data.pop(self.size)
+
+    def empty(self):
+        return self.size == 0
+
+    def top(self):
+        return self.data[self.size]
+
+    def clear(self):
+        self.data = []
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+class Queue:
+    def __init__(self, datas=[]):
+        self.data = list(datas)
+        self.size = len(self.data)
+
+    def push(self, num):
+        self.data.append(num)
+        self.size += 1
+
+    def pop(self):
+        if self.size == 0:
+            raise RuntimeError
+        self.size -= 1
+        return self.data.pop(0)
+
+    def empty(self):
+        return self.size == 0
+
+    def front(self):
+        return self.data[self.size]
+
+    def clear(self):
+        self.data = []
+        self.size = 0
+
+    def __len__(self):
+        return self.size
+
+class DisJointSet:  # 并查集类
+    def __init__(self, n):
+        self.fa = [i for i in range(0, n + 3)]
+
+    def father(self, x) -> int:
+        if self.fa[x] == x:
+            return x
+        self.fa[x] = self.father(self.fa[x])
+        return self.fa[x]
+
+    def merge(self, x, y):
+        xx = self.father(x)
+        yy = self.father(y)
+        if xx == yy:
+            return
+        self.fa[xx] = yy
+
+    def same(self, x, y) -> bool:
+        return self.father(x) == self.father(y)
+
+if __name__ == '__main__':
+    s = Stack([2,9,8])
+    s.push(3)
+    s.push(5)
+    t = s.pop()
+    while not s.empty():
+        print(s.pop(), end=" ")
+    print("")
+    s = Queue([2, 9, 8])
+    s.push(3)
+    s.push(5)
+    t = s.pop()
+    while not s.empty():
+        print(s.pop(), end=" ")
+
